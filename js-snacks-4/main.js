0664e3a187568024963fbc7819a7942943a58056
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-27 12:32:32 +0200

    complete ex

--- js-snacks-4/main.js
+++ js-snacks-4/main.js
@@ -1,7 +1,7 @@
-//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
-var username = prompt("Qual è il tuo nome?");
-var invitati = ["stefania","maria","marco","luigi"];
 //chiedi all’utente il suo nome 
+var username = prompt("Qual è il tuo nome?");
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+var invitati = ["stefania","maria","marco","luigi"];
 var accesso = false;
 //e comunicagli se può partecipare o no alla festa. 
 for (var i = 0; i < invitati.length; i++) {
