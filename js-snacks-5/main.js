201ce9bcfee03ebb748e14f120f34565d9e4b358
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-27 14:37:49 +0200

    complete ex

---
+++ js-snacks-5/main.js
@@ -0,0 +1,11 @@
+//Crea un array vuoto.
+var numberList = []
+//Chiedi per 6 volte all’utente di inserire un numero,
+for(i=0; i<6; i++){
+    var userNumber = Number(prompt("Inserisci un numero"));
+    //se è dispari inseriscilo nell’array.
+    if(userNumber % 2 !== 0){
+        numberList.push(userNumber);
+    }
+}
+console.log(numberList);
