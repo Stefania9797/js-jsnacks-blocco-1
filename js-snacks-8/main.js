fd28147334e774df48550db2f577c7b8cff34eaf
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-28 12:54:15 +0200

    correction

--- js-snacks-8/main.js
+++ js-snacks-8/main.js
@@ -3,7 +3,7 @@ var userNumber = Number(prompt("Inserisci un numero con 4 cifre"));
 //e calcola la somma di tutte le cifre che compongono il numero
 var somma= 0;
 while(userNumber){
-    somma = somma += userNumber % 10;
+    somma = somma + userNumber % 10;
     userNumber = Math.floor(userNumber / 10);
 }
 console.log(somma);
