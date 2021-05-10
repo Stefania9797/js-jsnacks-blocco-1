8463d97435da857fe6cef54e05b30e61d8c23955
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-10 11:33:35 +0200

    complete ex

---
+++ js-blocco-4/js-snack-1A/main.js
@@ -0,0 +1,5 @@
+//Creare un oggetto con le seguenti proprietà: nome:palla peso:10
+var palla= {
+    nome: "palla",
+    peso: 10
+}
--- js-snacks-3/main.js
+++ js-snacks-3/main.js
@@ -1,8 +1,18 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-var somma=0;
+/*var somma=0;
 for (i = 0; i<10; i++) {
     var userNumber = Number(prompt("Inserisci un numero"));
     somma += userNumber;
 }
 //Il programma stampa la somma di tutti i numeri inseriti
+console.log(somma);*/
+
+//ciclo while
+var somma = 0;
+i=0;
+while(i<10){
+    var userNumber = Number(prompt("Inserisci un numero"));
+    somma += userNumber;
+    i++;
+}
 console.log(somma);
